0b3619576069d51156aa0cb1f2e961a90db694ef
barbaraglopez <barbaralopezbbbb@gmail.com>
2024-02-07 16:05:07 -0300

    ajustes en el renderizado de estado de integraciones

--- src/pages/auth/Login/Login.jsx
+++ src/pages/auth/Login/Login.jsx
@@ -66,7 +66,7 @@ export const Login = () => {
                   value={values.email}
                   autoComplete="email"
                   onChange={handleChange}
-                  placeholder="  negocio@info.com"
+                  placeholder="negocio@info.com"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-2"
                 />
@@ -87,7 +87,7 @@ export const Login = () => {
                   name="password"
                   type="password"
                   value={values.password}
-                  placeholder="  ***********"
+                  placeholder="***********"
                   autoComplete="current-password"
                   onChange={handleChange}
                   required
--- src/pages/services/Configurations/EstadoBot.jsx
+++ src/pages/services/Configurations/EstadoBot.jsx
@@ -1,39 +1,42 @@
 import axios from "axios";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
-export const EstadoBot = () =>{
-    const [estadoBot, setEstadoBot]= useState({
-        appUptime: null,
-        whatsappConnectionStatus: null,
-        whatsappUptime: null
-      })
+export const EstadoBot = () => {
+  const [estadoBot, setEstadoBot] = useState({
+    appUptime: null,
+    whatsappConnectionStatus: null,
+    whatsappUptime: null,
+  });
 
-    const BASE_URL = "https://bot.yappastore.com";
-    const fetchEstadoDelBot = () => {
-        axios.get(`${BASE_URL}/info`)
-          .then(response => {
-            setEstadoBot(response.data)
-          })
-          .catch(error => console.error('Error al traer el estado del bot: ', error));
+  const BASE_URL = "https://bot.yappastore.com";
+
+  useEffect(() => {
+    const fetchEstadoDelBot = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/info`);
+        setEstadoBot(response.data);
+      } catch (error) {
+        console.error("Error al traer el estado del bot: ", error);
       }
+    };
 
-      useEffect(() => {
-        fetchEstadoDelBot();
-      }, []);
+    fetchEstadoDelBot();
+  }, []);
 
-
-
-    return (
-      <>
-        <div className="flex flex-col justify-center">
-          <div className="container flex flex-col items-center mt-5">
-            <p className="font-extrabold p-2 text-2xl max-lg:text-xl">
-              Estado de las integraciones
-            </p>
-            <div className="">{`Estado de coneccion : ${estadoBot.whatsappConnectionStatus}`}</div>
-            <div className="">{`Tiempo desconectado: ${estadoBot.appUptime}`}</div>
-            <div className="">{`Tiempo de coneccion: ${estadoBot.whatsappUptime}`}</div>
-          </div>
+  return (
+    <>
+      <div className="flex flex-col justify-center">
+        <div className="container flex flex-col items-center mt-5">
+          <p className="font-extrabold p-2 text-2xl max-lg:text-xl">
+            Estado de las integraciones
+          </p>
+          <div className="">{`Estado de conexión: ${estadoBot.whatsappConnectionStatus}`}</div>
+          <div className="">{`Tiempo desconectado: ${estadoBot.appUptime}`}</div>
+          {estadoBot.whatsappConnectionStatus === "conectado" && (
+            <div className="">{`Tiempo de conexión: ${estadoBot.whatsappUptime}`}</div>
+          )}
         </div>
-      </>
-    );}
+      </div>
+    </>
+  );
+};
